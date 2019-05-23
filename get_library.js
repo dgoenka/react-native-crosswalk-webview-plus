5492c9be4e76b4d8ed5a876aa90f0d8b9f354472
dgoenka <divyansh.goenka@gmail.com>
2019-05-23 14:43:40 +0530

    Update get_library.js

--- get_library.js
+++ get_library.js
@@ -30,7 +30,7 @@ function handleDownloaded (error, data) {
 }
 
 function downloadLibrary () {
-    var url = 'https://download.01.org/crosswalk/releases/crosswalk/android/maven2/org/xwalk/xwalk_core_library' + beta + '/' + version + '/xwalk_core_library' + beta + '-' + version +'-arm'+ '.aar';
+    var url = 'https://download.01.org/crosswalk/releases/crosswalk/android/maven2/org/xwalk/xwalk_core_library' + beta + '/' + version + '/xwalk_core_library' + beta + '-' + version +''+ '.aar';
     console.log('Downloading file...from'+url);
     wget(url, handleDownloaded);
 }
